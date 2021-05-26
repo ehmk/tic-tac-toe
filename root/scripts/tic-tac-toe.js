b73e9771cad2ce26fcf887bc8b09be7442be975e
Eric Hongmoungkhoune <ehmk90@gmail.com>
2021-05-26 17:59:18 -0400

    Appended the correct player's turn to the message panel.

--- root/scripts/tic-tac-toe.js
+++ root/scripts/tic-tac-toe.js
@@ -24,9 +24,11 @@ const playerTwo = playerFactory("Player two", "O");
 const game = (() => {
   let firstTurn = true;
   const switchTurns = () => {
+    const players = [playerOne, playerTwo];
     if (playerOne.getTurn() === false && playerTwo.getTurn() === false) {
-      playerOne.setTurn(true);
-      return playerOne;
+      let randomPlayer = players[Math.floor(Math.random() * players.length)];
+      randomPlayer.setTurn(true); // Gives a random player first turn
+      return randomPlayer;
     } else if (playerOne.getTurn() === true) {
       playerOne.setTurn(false);
       playerTwo.setTurn(true);
@@ -37,6 +39,13 @@ const game = (() => {
       return playerOne;
     }
   };
+  const getTurn = () => {
+    if (playerOne.getTurn() === true) {
+      return playerOne;
+    } else if (playerTwo.getTurn() === true) {
+      return playerTwo;
+    }
+  }
   const checkForWin = () => {
     if (gameBoard.getCoordinates()["a1"] === gameBoard.getCoordinates()["a2"] && gameBoard.getCoordinates()["a1"] === gameBoard.getCoordinates()["a3"] && gameBoard.getCoordinates()["a1"] !== "") {
       messageBoard.logWin();
@@ -58,30 +67,31 @@ const game = (() => {
       messageBoard.logTie();
     }
   }
-  return {switchTurns, checkForWin}
+  return {switchTurns, checkForWin, getTurn}
 })();
 
 const gameBoard = (() => {
   let _coordinates = {a1:"", a2:"", a3:"", b1:"", b2:"", b3:"", c1:"", c2:"", c3:""};
+  game.switchTurns();
   
   const a1 = document.querySelector("#a1");
-  a1.addEventListener('click', () => markCoordinate(game.switchTurns(), "a1", a1));
+  a1.addEventListener('click', () => markCoordinate(game.getTurn(), "a1", a1));
   const a2 = document.querySelector("#a2");
-  a2.addEventListener('click', () => markCoordinate(game.switchTurns(), "a2", a2));
+  a2.addEventListener('click', () => markCoordinate(game.getTurn(), "a2", a2));
   const a3 = document.querySelector("#a3");
-  a3.addEventListener('click', () => markCoordinate(game.switchTurns(), "a3", a3));
+  a3.addEventListener('click', () => markCoordinate(game.getTurn(), "a3", a3));
   const b1 = document.querySelector("#b1");
-  b1.addEventListener('click', () => markCoordinate(game.switchTurns(), "b1", b1));
+  b1.addEventListener('click', () => markCoordinate(game.getTurn(), "b1", b1));
   const b2 = document.querySelector("#b2");
-  b2.addEventListener('click', () => markCoordinate(game.switchTurns(), "b2", b2));
+  b2.addEventListener('click', () => markCoordinate(game.getTurn(), "b2", b2));
   const b3 = document.querySelector("#b3");
-  b3.addEventListener('click', () => markCoordinate(game.switchTurns(), "b3", b3));
+  b3.addEventListener('click', () => markCoordinate(game.getTurn(), "b3", b3));
   const c1 = document.querySelector("#c1");
-  c1.addEventListener('click', () => markCoordinate(game.switchTurns(), "c1", c1));
+  c1.addEventListener('click', () => markCoordinate(game.getTurn(), "c1", c1));
   const c2 = document.querySelector("#c2");
-  c2.addEventListener('click', () => markCoordinate(game.switchTurns(), "c2", c2));
+  c2.addEventListener('click', () => markCoordinate(game.getTurn(), "c2", c2));
   const c3 = document.querySelector("#c3");
-  c3.addEventListener('click', () => markCoordinate(game.switchTurns(), "c3", c3));
+  c3.addEventListener('click', () => markCoordinate(game.getTurn(), "c3", c3));
 
   const _selectors = [a1, a2, a3, b1, b2, b3, c1, c2, c3];
 
@@ -92,6 +102,8 @@ const gameBoard = (() => {
       mark.textContent = _coordinates[coordinate];
       selector.appendChild(mark);
       game.checkForWin();
+      game.switchTurns();
+      messageBoard.logTurn();
     }
   }
 
@@ -100,6 +112,10 @@ const gameBoard = (() => {
     for (let i = 0; i < _selectors.length; i++) {
       _selectors[i].textContent = "";
     }
+    playerOne.setTurn(false);
+    playerTwo.setTurn(false);
+    game.switchTurns();
+    messageBoard.logTurn();
   }
   return {
     getCoordinates() {
@@ -112,13 +128,16 @@ const gameBoard = (() => {
 const messageBoard = (() => {
   const messagePanel = document.querySelector("#message-panel");
   const resetButton = document.querySelector("#reset-game");
-  resetButton.addEventListener("click", gameBoard.resetBoard());
+  resetButton.addEventListener("click", () => {
+    messagePanel.textContent = (``)
+    gameBoard.resetBoard()
+  });
   const logWin = () => {
     if (playerOne.getTurn() === true) {
-      messagePanel.textContent = (`${playerOne.getName()} has won!`)
+      alert(`${playerOne.getName()} has won!`);
       gameBoard.resetBoard();
     } else {
-      messagePanel.textContent = (`${playerTwo.getName()} has won!`)
+      alert(`${playerTwo.getName()} has won!`);
       gameBoard.resetBoard();
     }
   };
@@ -126,8 +145,11 @@ const messageBoard = (() => {
     messageBoard.textContent = ("Draw");
     gameBoard.resetBoard();
   };
-
-  return {logWin, logTie}
+  const logTurn = () => {
+    messagePanel.textContent = `${game.getTurn().getName()}'s turn [${game.getTurn().getMark()}]`;
+  }
+  logTurn();
+  return {logWin, logTie, logTurn}
 })();
 
 
